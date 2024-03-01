978390d06744237f726f2c2839c247933a489122
Valentin Damyanov <valentin.damyanov@ampeco.global>
2024-03-01 08:40:17 +0200

    Add FetchInvoice and Update IssueInvoice functionality

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -8,6 +8,7 @@ class Response extends AbstractResponse
 {
     private int $code;
     const PURCHASE_RESPONSE_CODE_SUCCESS = 0;
+    const INVOICE_RESPONSE_CODE_SUCCESS = 0;
     const AUTHORIZE_RESPONSE_CODE_SUCCESS = 700;
     const AUTHORIZE_VALID_APPROVAL_RESPONSE_CODE_SUCCESS = 701;
 
@@ -27,10 +28,13 @@ class Response extends AbstractResponse
         return $this->code;
     }
 
+    public function getDocumentNumber()
+    {
+        return @$this->data['DocumentNumber'];
+    }
+
     public function isSuccessful(): bool
     {
         return $this->getCode() < 400 && $this->data['ResponseCode'] === self::PURCHASE_RESPONSE_CODE_SUCCESS;
     }
-
-
 }
