bbc52831a27c57b903f3602d230a6553981c5a3f
Valentin Damyanov <valentin.damyanov@ampeco.global>
2024-08-23 09:47:42 +0300

    add getToken method to FetchTransactionResponse

--- src/Message/FetchTransactionResponse.php
+++ src/Message/FetchTransactionResponse.php
@@ -19,4 +19,9 @@ class FetchTransactionResponse extends Response
     {
         return @$this->data['ApprovalNumber'];
     }
+
+    public function getToken()
+    {
+        return @$this->data['Token'];
+    }
 }
